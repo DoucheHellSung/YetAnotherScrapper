d92f9b7f3de777cae8c4994ef961796044ca214b
kunwa <kunwa@LAPTOP-O8VRS39A>
2022-03-17 19:55:01 +0530

    Add project files.

---
+++ YetAnotherScrapper/Configuration/Plugin Configuration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MediaBrowser.Model.Plugins;
+
+namespace YetAnotherScrapper.Configuration
+{
+    public class Plugin_Configuration : BasePluginConfiguration
+    {
+    }
+}
---
+++ YetAnotherScrapper/Plugin.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediaBrowser.Common.Plugins;
+using YetAnotherScrapper.Configuration;
+using MediaBrowser.Common.Configuration;
+using MediaBrowser.Model.Serialization;
+
+namespace YetAnotherScrapper
+{
+    public class Plugin : BasePlugin<Plugin_Configuration>
+    {
+        public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer) : base(applicationPaths, xmlSerializer)
+        {
+           
+        }
+
+        private Guid _id = new Guid("0A60512E-D75E-44E4-83CB-8B4A6ED5AC37");
+        public override Guid Id
+        {
+            get { return _id; }
+        }
+
+        public override string Name => throw new NotImplementedException();
+    }
+}
